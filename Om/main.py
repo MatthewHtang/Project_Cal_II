f9d7bc9d41bd2829329abeefce724c581b4c3ba0
MahatoOm <omprakashmahato236@gmail.com>
2025-10-06 21:32:45 -0500

    Commiting Files

---
+++ Om/main.py
@@ -0,0 +1,20 @@
+print("THis is cal 2 project")
+
+def integral():
+    coff = int(input("Enter cofficient of x "))
+    power = int(input("Enter power of x "))
+
+    print("The result is: \n")
+    print(f"{coff * (power+1)} X^{power + 1} / {power +1 }")
+    return 
+
+
+print(integral())
+
+'''
+
+ I x ^ 12
+
+== x * 13 / 13
+
+'''
