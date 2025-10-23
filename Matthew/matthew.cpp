db93d96d58b503bf8a920142d56c1b786e08a231
MatthewHtang <matthewhtang@Mac-948.lan>
2025-10-22 21:19:07 -0500

    differentiate function

--- Matthew/matthew.cpp
+++ Matthew/matthew.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
+
+//Function to Differentiate u
+
+string differentiate(string u)
+{
+    if (u == "x") return "1";
+    if (u == "x^2") return "2x";
+    if (u == "x^3") return "3x";
+    
+}
+
 int main()
 {
-    cout << "this is where we begin" << endl;
-    string sign;
-    cin >> sign;
-    cout << "your sign is" << sign << endl;
+    string u, dv;
 
-    return;
+    cout << "Integration by Parts Solver" << endl;
+    cout << "Formula : ∫u dv = u*v - ∫v du" << endl;
 
+    cout << "Enter u: ";
+    cin >> u;
+
+    cout << "Enter dv: ";
+    cin >> dv;
+
+    //handeling simple case//
+
+    if (u == "x" && dv == "e^x");
 }
